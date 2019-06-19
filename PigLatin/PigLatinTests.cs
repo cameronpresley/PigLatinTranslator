8f19091ea1276c0afc274c5153913a0974b0e570
Cameron Presley <Cameron@TheSoftwareMentor.com>
2019-06-19 14:11:56 -0400

    Initial implementation

---
+++ PigLatin/PigLatinTests.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PigLatin
+{
+    [TestClass]
+    public class PigLatinTests
+    {
+        [TestMethod]
+        public void WhenTranslatingASingleWordThenTheFirstLetterIsMovedToTheEndAndFormatted()
+        {
+            var inputs = new List<string> {"pig", "zillion"};
+            var expectedResults = new List<string> {"igpay", "illionzay"};
+
+            for (var i = 0; i < inputs.Count; i++)
+            {
+                var input = inputs[i];
+                var result = PigLatinTranslator.Translate(input);
+
+                Assert.AreEqual(expectedResults[i], result);
+            }
+        }
+
+        [TestMethod]
+        public void WhenTranslatingAndTheWordStartsWithAVowelThenAYIsAppended()
+        {
+            var inputs = new List<string> { "earring" };
+            var expectedResults = new List<string> { "earringay" };
+
+            for (var i = 0; i < inputs.Count; i++)
+            {
+                var input = inputs[i];
+                var result = PigLatinTranslator.Translate(input);
+
+                Assert.AreEqual(expectedResults[i], result);
+            }
+        }
+
+        [TestMethod]
+        public void WhenTranslatingAWordAndTheWordStartsWithAConsonantClusterThenTheClusterIsMovedToTheEndAndAYAppended()
+        {
+            var inputs = new List<string> { "clasp", "chrome"};
+            var expectedResults = new List<string> { "aspclay", "omechray"};
+
+            for (var i = 0; i < inputs.Count; i++)
+            {
+                var input = inputs[i];
+                var result = PigLatinTranslator.Translate(input);
+
+                Assert.AreEqual(expectedResults[i], result);
+            }
+        }
+
+        [TestMethod]
+        public void WhenTranslatingMultipleWordsThenEachWordIsTransformedCorrectly()
+        {
+            var inputs = new List<string> { "wear it with confidence" };
+            var expectedResults = new List<string> { "earway itay ithway onfidencecay" };
+
+            for (var i = 0; i < inputs.Count; i++)
+            {
+                var input = inputs[i];
+                var result = PigLatinTranslator.Translate(input);
+
+                Assert.AreEqual(expectedResults[i], result);
+            }
+        }
+
+        [TestMethod]
+        public void SingleWordCases()
+        {
+            var inputs = new List<string> { "Wear," };
+            var expectedResults = new List<string> { "Earway," };
+
+            for (var i = 0; i < inputs.Count; i++)
+            {
+                var input = inputs[i];
+                var result = PigLatinTranslator.Translate(input);
+
+                Assert.AreEqual(expectedResults[i], result);
+            }
+        }
+
+        [TestMethod]
+        public void WhenTranslatingMultipleWordsThenCapitilizationAndPunctuationIsMaintained()
+        {
+            var inputs = new List<string> { "Wear, it with confidence." };
+            var expectedResults = new List<string> { "Earway, itay ithway onfidencecay." };
+
+            for (var i = 0; i < inputs.Count; i++)
+            {
+                var input = inputs[i];
+                var result = PigLatinTranslator.Translate(input);
+
+                Assert.AreEqual(expectedResults[i], result);
+            }
+        }
+    }
+}
---
+++ PigLatin/PigLatinTranslator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PigLatin
+{
+    public static class PigLatinTranslator
+    {
+        public static string Translate(string input)
+        {
+            Func<string, string> map = s =>
+            {
+                return StartsWithVowel(s)
+                    ? s + "ay"
+                    : FormatForConsonantCluster(s);
+            };
+            Func<string, string> format = s =>
+            {
+                var punctuation = s.SkipWhile(x => !Char.IsPunctuation(x));
+                var removedPunctuation = punctuation.Any() ? s.Remove(s.Length - punctuation.Count()) : s;
+                var transformedWord = map(removedPunctuation);
+                transformedWord = transformedWord.ToLower();
+                if (Char.IsUpper(s[0]))
+                {
+                    transformedWord = Char.ToUpper(transformedWord[0]) + transformedWord.Substring(1);
+                }
+
+                return transformedWord + String.Join("", punctuation);
+            };
+            var formattedWords = input.Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(format);
+            return String.Join(" ", formattedWords);
+        }
+
+        // 1). Remove Punctuation (keeping track of what it is)
+        // 2). Keep track of the casing of the consonant cluster or vowel
+        // 3). Transform
+        // 4). ToLower the entire transformation
+        // 5). Update the casing of the first letter (if needed)
+        // 6). Add back punctuation
+
+        private static string FormatForConsonantCluster(string input)
+        {
+            var consonants = input.TakeWhile(x => !IsVowel(x));
+            return input.Substring(consonants.Count()) + new String(consonants.ToArray()) + "ay";
+        }
+
+        private static bool StartsWithVowel(string word)
+        {
+            return IsVowel(word[0]);
+        }
+
+        private static bool IsVowel(char letter)
+        {
+            return new List<char> {'a', 'e', 'i', 'o', 'u'}.Contains(letter);
+        }
+
+
+    }
+}
